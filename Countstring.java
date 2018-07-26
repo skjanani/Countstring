da201b2ec4757db8150e03c2f2eca3cec8730663
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-26 10:34:36 +0530

    Add files via upload

---
+++ Countstring.java
@@ -0,0 +1,19 @@
+package janani;
+import java.util.Scanner;
+public class Countstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str;
+int c=0,i;
+System.out.println("enter the string");
+Scanner ja=new Scanner(System.in);
+str=ja.nextLine();
+String s[]=str.split(" ");
+for(i=0;i<s.length;i++){
+	c=c+s[i].length();
+}
+System.out.println(c);
+	}
+
+}
